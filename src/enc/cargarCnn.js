620ae732291fdee5479f33b23d1ac0f7ccd97945
jesusQuintana-30-2017 <32751057+jesusQuintana-30-2017@users.noreply.github.com>
2022-01-10 10:26:33 -0600

    hola

--- src/enc/cargarCnn.js
+++ src/enc/cargarCnn.js
@@ -7,268 +7,37 @@ var conf = {
     {
       id: 1,
       select: false,
-      nombre: "10.230/10.245",
+      nombre: "10.145/10.144",
       canalDefault: "IBD",
       vers: "SDB-10",
       mysql: {
         crm:{
-          ip: "10.25.10.245",
+          ip: "10.25.10.144",
           usuario: "bswcrm_user",
           contrasena: "Entrada2020$",
           baseDatos: "siogen01",
         },
         cc:{    
-          ip: "10.25.10.230",
+          ip: "10.25.10.145",
           usuario: "bswcc_user",
           contrasena: "Entrada2020$",
           baseDatos: "asteriskcdrdb",
         },
         cco:{    
-          ip: "172.16.41.235",
+          ip: "10.25.10.145",
           usuario: "bswcc_user",
           contrasena: "Entrada2020$",
           baseDatos: "asteriskcdrdb",
         },
         mde: {
-          ip: "10.25.10.231",
+          ip: "10.25.10.144",
           usuario: "bswcrm_user",
           contrasena: "Entrada2020$",
           baseDatos: "inmc",
         }
       }
     },
-    {
-      id: 2,
-      select: false,
-      nombre: "10.234/10.245",
-      canalDefault: "IBD",
-      vers: "SDB-10-2",
-      mysql: {
-        crm:{
-          ip: "10.25.10.245",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "siogen01",
-        },
-        cc:{    
-          ip: "10.25.10.234",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        cco:{    
-          ip: "172.16.41.235",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        mde: {
-          ip: "10.25.10.231",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "inmc",
-        }
-      }
-    },
-    {
-      id: 3,
-      select: false,
-      nombre: "41.237/10.245",
-      canalDefault: "IBD",
-      vers: "SDB-10-4",
-      mysql: {
-        crm:{
-          ip: "10.25.10.245",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "siogen01",
-        },
-        cc:{    
-          ip: "172.16.41.237",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        cco:{    
-          ip: "172.16.41.235",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        mde: {
-          ip: "10.25.10.231",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "inmc",
-        }
-      }
-    },
-    {
-      id: 4,
-      select: false,
-      nombre: "41.230/41.245",
-      canalDefault: "IBD",
-      vers: "SDB-10",
-      mysql: {
-        crm:{
-          ip: "172.16.41.245",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "siogen01",
-        },
-        cc:{    
-          ip: "172.16.41.230",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        cco:{    
-          ip: "172.16.41.235",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        mde: {
-          ip: "172.16.41.240",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "inmc",
-        }
-      }
-    },
-    {
-      id: 5,
-      select: false,
-      nombre: "41.236/41.247",
-      canalDefault: "IBD",
-      vers: "SDB-10-3",
-      mysql: {
-        crm:{
-          ip: "172.16.41.247",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "siogen01",
-        },
-        cc:{    
-          ip: "172.16.41.236",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        cco:{    
-          ip: "172.16.41.229",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        mde: {
-          ip: "10.25.10.231",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "inmc",
-        }
-      }
-    },
-    {
-      id: 6,
-      select: false,
-      nombre: "41.238/41.247",
-      canalDefault: "IBD",
-      vers: "SDB-10-2",
-      mysql: {
-        crm:{
-          ip: "172.16.41.247",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "siogen01",
-        },
-        cc:{    
-          ip: "172.16.41.238",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        cco:{    
-          ip: "172.16.41.229",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        mde: {
-          ip: "10.25.10.231",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "inmc",
-        }
-      }
-    },
-    {
-      id: 7,
-      selet: false,
-      nombre: "41.230/41.247",
-      canalDefault: "IBD",
-      vers: "SDB-10",
-      mysql: {
-        crm:{
-          ip: "172.16.41.247",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "siogen01",
-        },
-        cc:{    
-          ip: "172.16.41.230",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        cco:{    
-          ip: "172.16.41.229",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        mde: {
-          ip: "10.25.10.231",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "inmc",
-        }
-      }
-    },
-    {
-      id: 8,
-      select: false,
-      nombre: "41.239/41.247",
-      canalDefault: "IBD",
-      vers: "SDB-10",
-      mysql: {
-        crm:{
-          ip: "172.16.41.247",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "siogen01",
-        },
-        cc:{    
-          ip: "172.16.41.239",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        cco:{    
-          ip: "172.16.41.229",
-          usuario: "bswcc_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "asteriskcdrdb",
-        },
-        mde: {
-          ip: "10.25.10.231",
-          usuario: "bswcrm_user",
-          contrasena: "Entrada2020$",
-          baseDatos: "inmc",
-        }
-      }
-    }
-    
+  
   ]
 }
 

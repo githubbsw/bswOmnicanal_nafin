995bbfe7477f6fe334b98616743bc0296819fd46
jesusQuintana-30-2017 <32751057+jesusQuintana-30-2017@users.noreply.github.com>
2024-03-03 18:38:31 -0600

    ajuste te tiempo

--- src/bk/querys/notificacionColaEspera.js
+++ src/bk/querys/notificacionColaEspera.js
@@ -1,3 +1,3 @@
 
-module.exports.consultQueueOut =  'SELECT btAgenteCmpId cmp FROM bstntrn.btagenteoutbound where btAgenteOutId = ?;';
+module.exports.consultQueueOut =  'SELECT btAgenteCmpId cmp FROM bstntrn.btagenteinbound where btAgenteInbId = ?;';
 module.exports.consultQueue =  'SELECT cuantos, numero FROM bstntrn.llamadaenespera where id = 999 and campania = ? ;';
